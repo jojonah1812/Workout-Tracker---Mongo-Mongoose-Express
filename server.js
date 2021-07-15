a27563ec7174b8ca03f5ba70db5bbabe34d883bb
Jonah <82416656+jojonah1812@users.noreply.github.com>
2021-07-15 12:35:49 -0700

    All routes complete.

--- server.js
+++ server.js
@@ -23,7 +23,7 @@ mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/workout", { use
 //     console.log(message);
 //   });
 
-  //POST-INSERT
+  //POST-INSERT - A POST route to create a workout
 app.post("/api/workouts", ({ body }, res) => {
   db.Workout.create(body)
       .then(dbWorkout => {
@@ -34,7 +34,7 @@ app.post("/api/workouts", ({ body }, res) => {
     });
 });
 
-//PUT-UPDATE
+//PUT-UPDATE - A PUT route to update a workout(HINT:you will have to find the workout by id and then push exercises to the exercises array)
 app.put("/api/workouts/:id", (req, res) => {
   db.Workout.findOneAndUpdate({}, { $push: { exercises: _id } }, { new: true })
     .then(dbUser => {
@@ -46,34 +46,63 @@ app.put("/api/workouts/:id", (req, res) => {
   });
 
 
-//GET
+//GET - A GET route to get the workouts(HINT: this will need an aggregate to add all the durations from each exercise together. Here is an example https://docs.mongodb.com/manual/reference/operator/aggregation/addFields/)
 app.get("/models/workouts", (req, res) => {
 
-    db.Workout.find({}
-      .then(dbWorkout => {
-        res.json(dbWorkout);
-        })
-      .catch(err => {
-        res.json(err);
-        })
-)})
+    db.Workout.aggregate([
+       {
+     //adds Duration tolist of fields - as noted as not being listed in the Model.js//
+        $addFields: {
+       totalDuration: { $sum: "exercises.$duration" }
+     }
+   }
+  ])
+    .then(dbWorkout => {
+      res.json(dbWorkout);
+      })
+    .catch(err => {
+      res.json(err);
+      })
+});
   
-  //GET with RANGE
-  app.get("/find/range", (req, res) => {
-    db.Workout.findOne(
-      { _id: mongojs.ObjectId(req.params.id)},
-      (error, data) => {
-        if (error) {
-          res.send(error);
-        } else {
-          res.send(data);
+  //GET - A GET route to get workouts in a specific range(HINT:very similar to the one above, but needs a limit. Here is an exampe https://kb.objectrocket.com/mongo-db/how-to-use-the-mongoose-limit-function-927)
+
+  //  -add aggregate
+  //  -add limit
+  //  -add closing error catches
+ 
+  // use range as a limit
+ 
+  app.get("/workouts/range", (req, res) => {
+    db.Workout.aggregate([
+      {
+        $addFields:{
+            totalDuration: {$sum: "exercises.duration"}
         }
       }
-    );
+    ])
+    .limit(5)
+    .then(dbWorkout => {
+      res.json(dbWorkout);
+    })
+    .catch(err => {
+        res.json(err);
+    })
   });
 
 //////////////////////
-// DELETE ROUTE NEEDED
+// DELETE ROUTE NEEDED - A DELETE route to delete a workout by a specific id
+// Note from Aidan TA: "_id": "6035819e069dac799094fff8",
+
+ProductModel.findOneAndDelete(
+  { _id: "6035819e069dac799094fff8" },
+  function (err) {
+    if (err) console.log(err);
+    console.log("Successful deletion");
+  }
+);
+
+
 
 
 app.listen(3000, () => {
