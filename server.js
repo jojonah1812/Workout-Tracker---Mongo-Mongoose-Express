16c3063e6e0c52c1f21f9376a1d37cb818b34a47
Jonah <82416656+jojonah1812@users.noreply.github.com>
2021-07-15 23:19:09 -0700

    added env

--- server.js
+++ server.js
@@ -2,9 +2,7 @@ const express = require("express");
 const mongoose = require("mongoose");
 const logger = require("morgan");
 const path = require("path"); 
-
-
-// const { Workout } = require("./models");
+require("dotenv").config();
 
 const PORT = process.env.PORT || 3000;
 const db = require("./models");
@@ -15,7 +13,15 @@ app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use(express.static("public"));
 
-mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/workout", { useNewUrlParser: true, });
+//connection: if no MONGODB_URI, it will go Local
+mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/workout", { useNewUrlParser: true, useFindAndModify: false});
+
+app.use(required("./routes/api.js"));
+app.use(required("./routes/view.js"));
+
+
+
+
 
 //Create Database?
 app.get("/", (req, res) => {
